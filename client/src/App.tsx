c40a76a7c920144a76815281167f3536d89db416
Pradnyesh2711 <100141334+Pradnyesh2711@users.noreply.github.com>
2023-09-16 14:42:11 +0530

    Contact us

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,12 @@ import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
 import HomeLayout from "layouts/home";
 import AboutUs from "layouts/about";
+import ContactUs from "layouts/contact";
 const App = () => {
   return (
     <Routes>
-<Route path="home/about" element={<AboutUs />} />
-   
+<Route path="home/about" element={<AboutUs/>} />
+<Route path="home/contact" element={<ContactUs/>} />
       <Route path="home/landing" element={<HomeLayout />} />
       <Route path="auth/*" element={<AuthLayout />} />
       <Route path="admin/*" element={<AdminLayout />} />
