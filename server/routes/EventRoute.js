0bbcae2a9a31fef241670058052924c2a737a2b0
Glen <dsouzaglen30@gmail.com>
2023-09-16 18:13:46 +0530

    .

--- server/routes/EventRoute.js
+++ server/routes/EventRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
-import {getByCity} from '../controllers/CityController';
-const router=express.Router();
+import { getByCity } from "../controllers/CityController";
+const router = express.Router();
 
-router.post('/getByCity',getByCity);
+router.post("/getByCity", getByCity);
 
 export default router;
