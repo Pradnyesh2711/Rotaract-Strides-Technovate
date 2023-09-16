8a9a52a8c8b60929fcb9de0d62450d8a659a180d
Glen <dsouzaglen30@gmail.com>
2023-09-16 12:57:21 +0530

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,11 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
-import ProtectedRoute from "layouts/ProtectedRoute";
+import HomeLayout from "layouts/home";
 const App = () => {
   return (
     <Routes>
+      <Route path="home/*" element={<HomeLayout />} />
       <Route path="auth/*" element={<AuthLayout />} />
       <Route path="admin/*" element={<AdminLayout />} />
       <Route
