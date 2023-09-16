059502e42ea5688e79f9808c6a7195c72d5116ca
Glen <dsouzaglen30@gmail.com>
2023-09-17 00:31:52 +0530

    .

--- server/routes/MemberRoute.js
+++ server/routes/MemberRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
-import MemberModel from "../models/memberModel";
+import Member from "../models/Member";
 
 const router = express.Router();
 
 router.get("/getAdmins", async (req, res) => {
   console.log("Hello");
   try {
-    const admins = await MemberModel.find({ type: "admin" });
+    const admins = await Member.find({ type: "admin" });
     res.status(200).json({ admins: admins });
   } catch (error) {
     res.status(500).json(error);
