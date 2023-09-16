3f337562630a3584fa3a5fc01375cff145516bcf
Kenneth <Kenneth012004@outlook.com>
2023-09-16 19:13:53 +0530

    Added edit functionality

--- client/src/components/sidebar/components/Links.tsx
+++ client/src/components/sidebar/components/Links.tsx
@@ -16,7 +16,7 @@ export const SidebarLinks = (props: { routes: RoutesType[] }): JSX.Element => {
 
   const createLinks = (routes: RoutesType[]) => {
     return routes.map((route, index) => {
-      if (route.layout === "/admin" || route.layout === "/station-admin") {
+      if (route.layout === "/admin" && route.path !== "editEvent") {
         return (
           <Link key={index} to={route.layout + "/" + route.path}>
             <div className="relative mb-3 flex hover:cursor-pointer">
--- client/src/views/admin/Dashboard/index.tsx
+++ client/src/views/admin/Dashboard/index.tsx
@@ -1,50 +1,127 @@
-import { MdReport } from "react-icons/md";
-import { FaTasks } from "react-icons/fa";
-import { TbReport } from "react-icons/tb";
+// src/Dashboard.tsx
+import React, { useState } from 'react';
+import { BsPencilFill } from "react-icons/bs";
+import { useNavigate } from 'react-router-dom';
 
-import Widget from "components/widget/Widget";
-import WeeklyIncidents from "./components/WeeklyIncidents";
-import { useAppSelector } from "app/store";
+interface EventState {
+    id:string;
+    eventType: string;
+    name: string;
+    date: Date;
+    location: string;
+    description: string;
+  }
 
-const Dashboard = () => {
-  const staff = useAppSelector((state) => state.staff.data);
-  const incidents = useAppSelector((state) => state.incidents.data);
+
+const Dashboard: React.FC = () => {
+    const d =new Date
+    const navigate = useNavigate();
+    const [selectedMonth, SetSelectedMonth] = useState(d.toLocaleString('en-US', { month: 'short' }))
+    const [eventList, SetEventList]= useState<Array<EventState>>([
+        {
+            id: "adasasjfjfs",
+            eventType: "Marathon",
+            name: "SPIT Marathon",
+            date: new Date(),
+            location: "Andheri, Mumbai",
+            description: "Join us for an exhilarating marathon experience that will test your endurance, determination, and love for running! The SPIT Marathon promises an unforgettable journey through scenic routes, camaraderie with fellow runners, and the thrill of crossing the finish line.",
+          },
+          {
+            id: "adasasjfjfs",
+            eventType: "Marathon",
+            name: "SPIT Marathon",
+            date: new Date(),
+            location: "Andheri, Mumbai",
+            description: "Join us for an exhilarating marathon experience that will test your endurance, determination, and love for running! The SPIT Marathon promises an unforgettable journey through scenic routes, camaraderie with fellow runners, and the thrill of crossing the finish line.",
+          },
+          {
+            id: "adasasjfjfs",
+            eventType: "Marathon",
+            name: "SPIT Marathon",
+            date: new Date(),
+            location: "Andheri, Mumbai",
+            description: "Join us for an exhilarating marathon experience that will test your endurance, determination, and love for running! The SPIT Marathon promises an unforgettable journey through scenic routes, camaraderie with fellow runners, and the thrill of crossing the finish line.",
+          },
+          {
+            id: "adasasjfjfs",
+            eventType: "Marathon",
+            name: "SPIT Marathon",
+            date: new Date(),
+            location: "Andheri, Mumbai",
+            description: "Join us for an exhilarating marathon experience that will test your endurance, determination, and love for running! The SPIT Marathon promises an unforgettable journey through scenic routes, camaraderie with fellow runners, and the thrill of crossing the finish line.",
+          },
+          {
+            id: "adasasjfjfs",
+            eventType: "Marathon",
+            name: "SPIT Marathon",
+            date: new Date(),
+            location: "Andheri, Mumbai",
+            description: "Join us for an exhilarating marathon experience that will test your endurance, determination, and love for running! The SPIT Marathon promises an unforgettable journey through scenic routes, camaraderie with fellow runners, and the thrill of crossing the finish line.",
+          },
+          
+    ])
+    const monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
 
   return (
-    <div>
-      {/* Card widget */}
-
-      <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3">
-        <Widget
-          icon={<TbReport className="h-7 w-7" />}
-          title={"Reported Incidents"}
-          subtitle={"33"}
-        />
-        <Widget
-          icon={<FaTasks className="h-6 w-6" />}
-          title={"Completed Tasks"}
-          subtitle={"23"}
-        />
-        <Widget
-          icon={<MdReport className="h-7 w-7" />}
-          title={"Detected Incidents"}
-          subtitle={"13"}
-        />
-      </div>
-
-      {/* Charts */}
-
-      <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2">
-        <WeeklyIncidents />
-      </div>
-
-      {/* Tables & Charts */}
-
-      <div className="col-span-2 mt-5 grid grid-cols-2 gap-5 md:grid-cols-3 ">
-        {/* Check Table */}
-        <div className="col-span-2"></div>
-        <div className="grid grid-cols-1 rounded-[20px]"></div>
-      </div>
+    <div className="h-max">
+        <div className='bg-white mt-6 rounded-xl px-16 py-6 shadow-2xl'>
+        <h2 className="font-bold capitalize text-navy-700 dark:text-white text-3xl">My Marathons</h2>
+        <div className='grid grid-cols-12 my-6'>
+            {monthList.map((month)=>
+            {
+                return (
+                        month == selectedMonth? <button className='bg-yellow-600 px-6 py-3 mx-1 rounded-md '>
+                    {month}
+                    </button>:<button className='bg-yellow-200 px-6 py-3 mx-1 rounded-md ' onClick={( )=>
+                    {
+                        SetSelectedMonth(month)
+                    }}>
+                    {month}
+                    </button>
+                   
+                )
+            }
+                )}
+        </div>
+             <div>
+                {eventList.filter((e)=>
+                
+                    selectedMonth == monthList[e.date.getMonth()]
+                ).map((event) =>
+                (
+                    <div className='bg-blue-200 bordersolid my-3 rounded-md py-3 px-6'>
+                    <div className='grid grid-cols-2'>
+                      <h4 className="font-bold capitalize text-navy-700 dark:text-white text-lg"> {event.eventType} ({event.location})</h4>
+                      <div className='text-right'><button className='bg-navy-500 p-2 mx-1 rounded-md inline text-white ' onClick={( )=>
+                      {
+                        navigate('/admin/editevent',{state:{event:event}});
+                      }}><BsPencilFill/></button>
+                        </div>
+                      
+                    
+                    <h5 className="font-bold capitalize text-navy-500 dark:text-white text-lg">{event.name}</h5>
+                    <h5 className="font-bold capitalize text-navy-500 dark:text-white text-lg">{event.date.toDateString()}</h5>
+        
+                    </div>
+                    <p className='font-bold text-navy-400 dark:text-white text-sm my-2'>
+                        {event.description}
+                    </p>
+                    </div>
+                ))}
+                {eventList.filter((e)=>
+                
+                selectedMonth == monthList[e.date.getMonth()]
+            ).length?null:
+            <div className='bg-blue-200 bordersolid my-3 rounded-md py-3 px-6'>
+                <h4 className="font-bold capitalize text-navy-700 dark:text-white text-lg">No Events</h4>
+                   
+                </div>
+        
+            }
+                </div>
+        
+       
+        </div>
     </div>
   );
 };
