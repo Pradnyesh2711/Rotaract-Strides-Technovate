3256e802e04b05c3f2a49b9a74ee46f52947328e
Glen <dsouzaglen30@gmail.com>
2023-09-17 05:45:05 +0530

    Merge branch 'master' of https://github.com/GlenDsza/Bit-masters

--- server/routes/DemberRoute.js
+++ server/routes/DemberRoute.js
@@ -4,10 +4,19 @@ import DemberModel from "../models/demberModel.js";
 const router = express.Router();
 
 router.get("/getAdmins", async (req, res) => {
-  console.log("Hello");
   try {
     const admins = await DemberModel.find({ type: "admin" });
     res.status(200).json({ admins: admins });
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error);
+  }
+});
+
+router.get("/getAllMembers", async (req, res) => {
+  try {
+    const members = await DemberModel.find();
+    res.status(200).json({ members });
   } catch (error) {
     res.status(500).json(error);
     console.log(error);
