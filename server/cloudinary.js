1cfb5cf66c368d1785d3bf130d969e1afc8f6ed2
Pradnyesh2711 <100141334+Pradnyesh2711@users.noreply.github.com>
2023-09-17 06:46:17 +0530

    Eventslice

---
+++ server/cloudinary.js
@@ -0,0 +1,10 @@
+const cloudinary=require('cloudinary').v2;
+cloudinary.config({
+cloud_name:'dvxacup6m',
+api_key:'995456639814318',
+api_secret:'Jg1ZUDlOfOKBU5oMb2PHcpPbYQY',
+
+secure:true
+})
+
+export default cloudinary;
